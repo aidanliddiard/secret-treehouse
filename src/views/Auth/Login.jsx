4adb9720f922ea18b7a5ee925d6f46c23bfb2d94
aidanliddiard <aidanliddiard@gmail.com>
2022-05-04 12:41:02 -0700

    test passing

--- src/views/Auth/Login.jsx
+++ src/views/Auth/Login.jsx
@@ -42,18 +42,20 @@ export default function Login() {
     <>
       <h3>You must log in to view the page at {from.pathname}</h3>
       <form onSubmit={handleLogin} className={styles.loginForm}>
-        <label>Email</label>
+        <label htmlFor="email">Email</label>
         <input
           id="email"
           name="email"
           type="email"
+          value={formState.email}
           onChange={(event) => handleFormChange(event)}
         />{' '}
-        <label>Password</label>
+        <label htmlFor="password">Password</label>
         <input
           id="password"
           name="password"
           type="password"
+          value={formState.password}
           onChange={(event) => handleFormChange(event)}
         />
         <button type="submit" aria-label="Sign In">
